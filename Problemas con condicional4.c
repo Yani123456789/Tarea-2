ccd83b81b3a7d5899ab0da658ab3ea105088c410
Yani123456789 <yaniarrazola54@gmail.com>
2026-05-07 13:42:36 -0600

    Create Problemas con condicional4.c

---
+++ Problemas con condicional4.c
@@ -0,0 +1,20 @@
+//Construir un diagrama de flujo que queda determinar,
+//dados dos números enteros, si un nùmero es divisor del otro.
+
+//Construir un diagrama de flujo que queda determinar,
+//dados dos números enteros, si un nùmero es divisor del otro.
+
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    double mod;
+    
+    printf("Escribe el valor del primer número entero:");
+    scanf("%i", &a);
+    printf("Escribe el valor del segundo número entero:");
+    scanf("%i", &b);
+    
+    mod=a%b
+    
+}
